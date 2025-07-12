7bc33447b344de6d91dfc4acde76f5c13b28a499
AliAshour2 <aliashour468@gmail.com>
2025-07-12 22:23:28 +0300

    feat(product-detail): implement product detail modal and enhance product card interaction
    - Added a product detail modal to display comprehensive product information when the product card is clicked.
    - Updated product card component to open the modal on click and prevent event propagation for the "Add to Cart" button.
    - Introduced a new product detail component with structured layout and action buttons for adding to cart and closing the modal.
    - Included custom styles for the product detail component to improve visual presentation.

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -4,19 +4,31 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
 import { TruncatePipe } from '../../pipes/truncate-pipe/truncate.pipe';
 import { HoverDirective } from '../../shared/directives/hover/hover.directive';
 import { CartService } from '../../services/cart/cart.service';
+import { ModalComponent } from '../../shared/components/modal/modal/modal.component';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-card',
-  imports: [CommonModule  , CurrencyPipe , TruncatePipe,HoverDirective],
+  imports: [CommonModule, CurrencyPipe, TruncatePipe, HoverDirective, ModalComponent, ProductDetailComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-  @Input() product! : Product ;
+  @Input() product!: Product;
+  
+  isModalOpen = false;
 
   constructor(private cartService: CartService) {}
 
   addToCart() {
     this.cartService.addToCart(this.product);
+  }
+
+  openProductModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
   }
 }
---
+++ src/app/components/product-detail/product-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Product } from '../../models/card.model';
+import { CartService } from '../../services/cart/cart.service';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule, CurrencyPipe],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.css'
+})
+export class ProductDetailComponent {
+  @Input() product!: Product;
+  @Output() closeModal = new EventEmitter<void>();
+  
+  constructor(private cartService: CartService) {}
+  
+  addToCart() {
+    this.cartService.addToCart(this.product);
+    this.closeModal.emit();
+  }
+} 
