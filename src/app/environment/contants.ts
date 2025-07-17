0da04bf0adbf50333d44587393382515077320b0
AliAshour2 <aliashour468@gmail.com>
2025-07-17 10:18:56 +0300

    refactor(product-card): enhance button styling and update API endpoint constants
    - Updated the "Details" button in the product card component to improve styling with margin and padding adjustments.
    - Refactored API endpoint constants for better readability and consistency, ensuring proper formatting and adding a new endpoint for fetching all products.

--- src/app/environment/contants.ts
+++ src/app/environment/contants.ts
@@ -1,21 +1,22 @@
-export const BaseUrlFake =  "https://fakestoreapi.com/"
+export const BaseUrlFake = "https://fakestoreapi.com/"
 
 // src/app/core/constants/api-endpoints.const.ts
 export const API_ENDPOINTS = {
-    PRODUCTS: {
-      BASE: '/products',
-      SINGLE : (id : string)=>`/products/${id}`,
-      CATEGORIES: '/products/categories',
-      CATEGORY: (category: string) => `/products/category/${category}`
-    },
-    CARTS: {
-      BASE: '/carts',
-      USER: (userId: number) => `/carts/user/${userId}`
-    },
+  PRODUCTS: {
+    BASE: '/products',
+    SINGLE: (id: string) => `/products/${id}`,
+    CATEGORIES: '/products/categories',
+    CATEGORY: (category: string) => `/products/category/${category}`
+  },
+  CARTS: {
+    BASE: '/carts',
+    USER: (userId: number) => `/carts/user/${userId}`
+  },
 
-  };
+};
 
 
-  export const BaseUrl  = "https://ecommerce.routemisr.com/api/v1/"
-  export const SignUpEndPoint = `${BaseUrl}auth/signup`
-  export const SingInEndPoint = `${BaseUrl}auth/signin`
+export const BaseUrl = "https://ecommerce.routemisr.com/api/v1"
+export const SignUpEndPoint = `${BaseUrl}/auth/signup`
+export const SingInEndPoint = `${BaseUrl}/auth/signin`
+export const GetAllProductEndPoint = `BaseUrl/products`
